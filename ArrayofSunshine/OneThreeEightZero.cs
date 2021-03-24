2bbfb47d9e7f3413f9bd34b4de8248b509335a9e
Brian Irving <brianirv@buffalo.edu>
2021-03-24 06:47:28 -0600

    Complete Rework of Lucky number algorithm

--- ArrayofSunshine/OneThreeEightZero.cs
+++ ArrayofSunshine/OneThreeEightZero.cs
@@ -14,50 +14,86 @@ namespace ArrayofSunshine
             //hold on to it(?) and compare it to the other nums in the Y coloumn
             //return whichever is 
             List<int> test = new List<int>();
-          
-           
-          
             bool searching = true;
-
+            int row = 0;
+           
+            int count = 0;
             while (searching)
             {
-                int max = matrix[0][0];
-                int xLocator = 0;
-                int yLocator = 0;
-                for (int i = 0; i < matrix.Length; i++)
+                int min = matrix[row][0];
+
+                //if its the smallest in its column move to step 2
+                //if it is not, then move over to the next value in the row and repeat.
+                //if none in that row satisfy, +1 the row. repeat step 1. You don't need to compare to the previous row these will be bigger logically
+                for (int col = 0; col <= matrix[row].Length-1; col++)
                 {
-                    for (int j = 1; j < matrix.Length; j++)
+
+                   
+                    if (matrix[row][col] < min)
+                    //matrix[col is constant][row is iterator]
                     {
-                        test.Add(matrix[i][j]);
+                        
+                        //seeing if num is SMALLEST IN THE ROW
+                        //to look thru row, you're changing the var COL
+
+                        min = matrix[row][col];
+                        count = col;
+                    }
+                     
+                }
+                //2. Found smallest. Is it the biggest in it's column? Iterate thru column with matrix[j(iterator will be col)][row will be constant]
+                //if its the biggest in its row, add it to the list, otherwise +1 to row and start over again. 
+
+                int minimum = min;
+              
+                int max = min;
+
+                foreach(int[] element in matrix)
+               
+                  
+                    //to iterate thru column to find biggest number CHANGE THE ROW VAL
+                    if (element[count]>min)
+                    {
+                        //if you find a num in the column bigger than our current target(min)
+                        //set it equal to test condition
+                        //jump to the next ROW 
+
+                        max = element[count];
+                        row++;
+
+                        //if row is not equal to the matrix's length, break out and repeat 
+                        if (row != matrix.Length)
+                        {
+                            searching = true;
+                          
+                            break;
+                        }
+ 
                     }
 
+                
+                //if the min val hasn't changed since the second loop/it is also the max in its column-add it to the list
+                if (max == min)
+                {
+                    test.Add(min);
+                    row++;
                 }
-    //
-    //        //now that we've found the biggest int we need to figure out if its the smallest in the row
-    //        foreach(int num in matrix[yLocator])
-    //        {
-    //            //if true then this num isn't lucky, zero it out
-    //            if (num < max)
-    //            {
-    //                matrix[xLocator][yLocator] = 0;
-    //            }
-    //            else
-    //            {
-    //                searching = true;
-    //                int luckyNumber = max;
-    //                test.Add(max);
-    //            }   
-    //        }
-    //    }
+                
+                //regardless if the row # equals the length of the matrix, we're at the last row and we need to break out of our while condition
+                if (row == matrix.Length)
+                {
+                    searching = false;
+                    return test;
+                }
+                min = 0;
+                max = 0;
+             
+            }
 
-            return test;
-
+            return test;  //if its the smallest in its row
         }
-
     }
 }
-    }
-
 ////Given a m * n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
 //
 //A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
--- ArrayofSunshine/Program.cs
+++ ArrayofSunshine/Program.cs
@@ -12,9 +12,9 @@ namespace ArrayofSunshine
         static void Main(string[] args)
         {
             int[][] matrix = new int[3][];
-            matrix[0] = new int[3] { 3, 7, 8 };
-            matrix[1] = new int[3] { 9, 11, 13 };
-            matrix[2] = new int[3] { 15, 16, 17 };
+            matrix[0] = new int[4] { 1, 10, 4, 2 };
+            matrix[1] = new int[4] { 9, 3, 8, 7 };
+            matrix[2] = new int[4] { 15, 16, 17, 12 };
             OneThreeEightZero ln = new OneThreeEightZero();
             ln.LuckyNumbers(matrix);
 
